ec9d0b0e0e6696d99dd799e3778cf9aac66a84a2
Nazmul Hoque <coder.nazmul@gmail.com>
2020-07-07 11:20:31 +0600

    form elements done

--- src/Component/Form/Form.js
+++ src/Component/Form/Form.js
@@ -6,13 +6,31 @@ class Form extends React.Component {
         name: "",
         country: "",
         bio: "",
-        birthdate: ""
+        birthdate: "",
+        gender: "",
+        agree: false,
+        skills: []
     }
     handleChange = (event) => {
         this.setState({[event.target.name] : event.target.value})
     }
     handleClick = () => {
         console.log(this.state);
+    }
+    handleCheckbox = (event) => {
+        this.setState({
+            agree: event.target.checked
+        });
+    }
+    handleSkills = (event) => {
+        if (event.target.checked) {
+            this.setState({
+                skills: [...this.state.skills, event.target.value]
+            });
+        } else {
+            const skills = this.state.skills.filter(skill => skill !== event.target.value);
+            this.setState({skills});
+        }
     }
     render () {
         return(
@@ -24,6 +42,7 @@ class Form extends React.Component {
                 placeholder="Type your name"
                 onChange={this.handleChange}
                 />
+
                 <select name="country" onChange={this.handleChange}>
                     <option>Select Your</option>
                     <option value="bangladesh">Bangladesh</option>
@@ -31,16 +50,71 @@ class Form extends React.Component {
                     <option value="india">India</option>
                     <option value="srilanka">Srilanka</option>
                 </select>
+
                 <textarea 
                 name="bio" 
                 placeholder="Type your Bio"
                 onChange={this.handleChange}
                 ></textarea>
+
                 <input 
                 type="date" 
                 name="birthdate"
                 onChange={this.handleChange}
                 />
+
+                <input 
+                type="radio" 
+                name="gender" 
+                value="male" 
+                onChange={this.handleChange}/>
+                Male
+                <input 
+                type="radio" 
+                name="gender" 
+                value="female" 
+                onChange={this.handleChange}/>
+                Female
+
+                <div style={{ margin: '20px 0px' }}>
+                    <label htmlFor="#agree">Are You Agree?</label>
+                    <input 
+                    type="checkbox"
+                    id="agree" 
+                    name="agree" 
+                    checked={this.state.agree}
+                    onChange = {this.handleCheckbox}
+                    />
+                </div>
+
+                <div style={{ margin: '20px 0px' }}>
+                    <label htmlFor="#agree">Skills:- </label>
+                    <input 
+                    type="checkbox"
+                    name="skills" 
+                    value="PHP"
+                    checked={this.state.skills.includes("PHP")}
+                    onChange = {this.handleSkills}
+                    />
+                    PHP
+                    <input 
+                    type="checkbox"
+                    name="skills" 
+                    value="JavaScript"
+                    checked={this.state.skills.includes("JavaScript")}
+                    onChange = {this.handleSkills}
+                    />
+                    JavaScript
+                    <input 
+                    type="checkbox"
+                    name="skills" 
+                    value="Python"
+                    checked={this.state.skills.includes("Python")}
+                    onChange = {this.handleSkills}
+                    />
+                    Python
+                </div>
+
                 <button onClick={this.handleClick}>Show Data</button>
             </div>
         );
