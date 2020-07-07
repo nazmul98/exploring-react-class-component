6aedae3b3d35310c8367e69a0bb7cf3054a8d3a5
Nazmul Hoque <coder.nazmul@gmail.com>
2020-07-07 21:40:48 +0600

    connected redux via react-redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+// Connect child Component
 import Timer from './Component/Timer/Timer';
 import Event from './Component/Event/Event';
 import ReduxConcept from "./Component/ReduxConcept/ReduxConcept";
 import UncontrolledForm from './Component/UncontrolledForm/UncontrolledForm';
 import ControlledForm from './Component/Controlled/Controlled';
+// for connecting redux
+import { Provider } from 'react-redux';
+import store from "./store";
+// react router
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,32 +20,34 @@ import Form from './Component/Form/Form';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Router>
-            <div>
-              <Switch>
-                <Route path="/form">
-                  <Form />
-                </Route>
-                <Route path="/uncform">
-                  <UncontrolledForm />
-                </Route>
-                <Route path="/controlled">
-                  <ControlledForm />
-                </Route>
-                <Route path="/simpleredux">
-                  <ReduxConcept />
-                </Route>
-                <Route path="/">
-                  <Timer />
-                  <Event />
-                </Route>
-              </Switch>
-            </div>
-          </Router>
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <Router>
+              <div>
+                <Switch>
+                  <Route path="/form">
+                    <Form />
+                  </Route>
+                  <Route path="/uncform">
+                    <UncontrolledForm />
+                  </Route>
+                  <Route path="/controlled">
+                    <ControlledForm />
+                  </Route>
+                  <Route path="/simpleredux">
+                    <ReduxConcept />
+                  </Route>
+                  <Route path="/">
+                    <Timer />
+                    <Event />
+                  </Route>
+                </Switch>
+              </div>
+            </Router>
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
